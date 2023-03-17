e91cf143da67e0111c7486851f6eb6779f140e1d
Tunahan Akça <94146370+tunhnakca@users.noreply.github.com>
2023-03-17 11:31:41 +0300

    Unique In Order

---
+++ uniqueInOrder.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var uniqueInOrder = function (iterable) {
+  const arr = [];
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== iterable[i + 1]) arr.push(iterable[i]);
+  }
+  return arr;
+};
